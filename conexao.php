82d29473fd6b256cac8f6e3fafe8556b19996f69
WendellFerreira <86381991+WendellFerreira@users.noreply.github.com>
2024-06-06 18:53:31 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -11,7 +11,7 @@ $dsn = "mysql:host=$host; dbname=$db;charset=$charset";
 try{
 $pdo = new PDO($dsn, $user, $pass);
 } catch(PDOException $e){
-    echo "Erro ao tentar conectar com o banco de dados <p>" . $e;
+    echo "Erro ao tentar conectar com o banco de dados" . $e;
 }
 echo "Conexão funcionando";
 ?>
